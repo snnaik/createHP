186e0b3491effae94c2eea3b849c1d5a6514746f
Sharvil Naik <sharvil.naik@macys.com>
2016-04-03 14:22:15 -0700

    F4.3 modularize tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function(grunt) {
 
 	grunt.loadTasks("tasks");
 	grunt.registerTask("template", ["check", "build", "execute"]);
-	grunt.registerTask("reformat", ["check", "update", "jsp", "extract", "clean", "finalize"]);
+	grunt.registerTask("reformat", ["check", "update", "prepare", "finalize"]);
 
 	grunt.registerTask("check", function() {
 		!grunt.option("folder") ? grunt.fatal("Folder parameter missing!\n") : grunt.config.set("vars.folder", (folder = grunt.option("folder").toString()));
@@ -59,53 +59,6 @@ module.exports = function(grunt) {
 		})
 		grunt.loadNpmTasks('grunt-prettify');
 		grunt.task.run('prettify');
-	});
-
-	grunt.registerTask("jsp", function() {
-		this.requires("update");
-
-		var $ = grunt.config.get("vars.$"),
-			jsp = grunt.file.exists("assets/jsp_directive.txt") ? grunt.file.read("assets/jsp_directive.txt") : "",
-			content = $("head").html();
-
-		if(/macy-base/.test(content)) content = content.replace(/<link.*/, "");
-		if(/jquery/.test(content)) content = content.replace(/<script.*\n.*/, "");
-
-		content = jsp.concat(content, $("body").html());
-
-		grunt.file.write(grunt.config.get("vars.folder") + grunt.config.get("vars.files.hp2"), content);
-	});
-
-	grunt.registerTask("extract", function() {
-		this.requires("jsp");
-
-		var imgAlt = "", areaAlt = "", $ = grunt.config.get("vars.$");
-
-		$("img").each(function() {
-			imgAlt += $(this).attr("alt") + "\n";
-		});
-		$("area").each(function() {
-			areaAlt += $(this).attr("alt") + "\n";
-		});
-		grunt.file.write(folder + files.alt, imgAlt + areaAlt);
-	});
-
-	grunt.registerTask("clean", function() {
-		this.requires("extract");
-
-		var lines = grunt.file.read(folder + files.hp2).split("\n"),
-			newlines = [],
-			i = lines.length, line;
-
-		while(i--) {
-			line = /^(<img|<area).*(?!\/>)$/.test(lines[i].trim()) ? lines[i].replace(/>$/, "/>") : lines[i];
-			if(/&amp;/.test(line)) line = line.replace(/&amp;/g, "&");
-			if(/(&apos;|&quot;|&#x2019;)/.test(line)) line = line.replace(/(&apos;|&quot;|&#x2019;)/g, "'");
-			if(/&#xA0;/.test(line)) line = line.replace(/&#xA0;/g, " ");
-			line = line.replace(/^\t{1,2}/, "");
-			newlines[i] = line;
-		}
-		grunt.file.write(folder + files.hp2, newlines.join("\n"));
 	});
 
 	grunt.registerTask("finalize", function() { return true;
---
+++ tasks/prepare.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+	var folder, files;
+
+	grunt.registerTask("prepare", ["jsp", "extract", "clean"]);
+
+	grunt.registerTask("jsp", function() {
+		this.requires("update");
+
+		var $ = grunt.config.get("vars.$"),
+			jsp = grunt.file.exists("assets/jsp_directive.txt") ? grunt.file.read("assets/jsp_directive.txt") : "",
+			content = $("head").html();
+
+		files = grunt.config.get("vars.files");
+		folder = grunt.config.get("vars.folder");
+
+		if(/macy-base/.test(content)) content = content.replace(/<link.*/, "");
+		if(/jquery/.test(content)) content = content.replace(/<script.*\n.*/, "");
+
+		content = jsp.concat(content.trim(), $("body").html());
+
+		grunt.file.write(folder + files.hp2, content);
+	});
+
+	grunt.registerTask("extract", function() {
+		this.requires("jsp");
+
+		var imgAlt = "", areaAlt = "", $ = grunt.config.get("vars.$");
+
+		$("img").each(function() {
+			imgAlt += $(this).attr("alt") + "\n";
+		});
+		$("area").each(function() {
+			areaAlt += $(this).attr("alt") + "\n";
+		});
+		grunt.file.write(folder + files.alt, imgAlt + areaAlt);
+	});
+
+	grunt.registerTask("clean", function() {
+		this.requires("extract");
+
+		var file = grunt.file.read(folder + files.hp2),
+			newlines = [], lines, line, i;
+
+		if(/&amp;/.test(file)) file = file.replace(/&amp;/g, "&");
+		if(/(&apos;|&quot;|&#x2019;)/.test(file)) file = file.replace(/(&apos;|&quot;|&#x2019;)/g, "'");
+		if(/&#xA0;/.test(file)) file = file.replace(/&#xA0;/g, " ");
+
+		lines = file.split("\n");
+		i = lines.length;
+
+		while(i--) {
+			line = /^(<img|<area).*(?!\/>)$/.test(lines[i].trim()) ? lines[i].replace(/>$/, "/>") : lines[i];
+			line = line.replace(/^\t{1,2}/, "");
+			newlines[i] = line;
+		}
+		grunt.file.write(folder + files.hp2, newlines.join("\n"));
+	});
+};
