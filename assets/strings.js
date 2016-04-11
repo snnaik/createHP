28947f0fe54bbf265b524878e915370c63b5ead6
Sharvil Naik <sharvil.naik@macys.com>
2016-04-11 13:06:33 -0700

    generic css update

--- assets/strings.js
+++ assets/strings.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"link" : '<link rel="stylesheet" href="../assets/macy-base.css" type="text/css" />',
 	"jq" : '<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"> </script>',
-	"genCss" : ".column, .columns {padding: 0px !important;}",
+	"genCss" : ".column, .columns {padding: 0px !important;} #globalContentContainer { border: none;}",
 	"blockCss" : ".block_grid ul, .block_grid li {margin: 0px !important;padding: 0px !important}",
 	"xtraWideCss" : ".xtraWideImg {max-width: none !important;margin-left: -50% !important; width: auto !important;} #doc3 {overflow-x: hidden !important; min-width: 960px;}"
 };
