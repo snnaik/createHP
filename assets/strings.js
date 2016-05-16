4df9b4fe69694fcacd48adfc3b1fbb3a73b60291
Sharvil Naik <sharvil.naik@macys.com>
2016-05-16 12:42:26 -0700

    css update for block grids

--- assets/strings.js
+++ assets/strings.js
@@ -2,7 +2,7 @@ module.exports = {
 	"link" : '<link rel="stylesheet" href="../assets/macy-base.css" type="text/css" />',
 	"jq" : '<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"> </script>',
 	"genCss" : "#globalContentContainer { border: none !important;}",
-	"blockCss" : ".block_grid ul, .block_grid li {margin: 0px !important;padding: 0px !important;}",
+	"blockCss" : ".block_grid ul, .block_grid li {margin: 0px !important;padding: 0px !important;} #globalContentContainer [class^='small-block-grid-'] > li {width: auto}",
 	"xtraWideCss" : ".xtraWideImg {max-width: none !important;margin-left: -50% !important; width: auto !important;} #doc3 {overflow-x: hidden !important; min-width: 960px;}",
 	"clickCopyCss" : ".clickCopyDiv {position: absolute; width: 85px; height: 23px; left: 843px; top: 165px; }"
 };
